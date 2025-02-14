62aa05db46ac337e0da4979e98c09d5600fda00e
ivanmateu <iesvda.ivamai@gmail.com>
2025-02-14 10:19:07 +0100

    Add authentication UI

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://nrkzjbojqyzsvncvgbwt.supabase.co/Projecte';
-const supabaseKey = 'https://nrkzjbojqyzsvncvgbwt.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ya3pqYm9qcXl6c3ZuY3ZnYnd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5MTg3NzksImV4cCI6MjA1NDQ5NDc3OX0.QScMS0B1RvaIgh3rm4shqyaif2PB6xwD-QWHub12oAM';
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 const emailInput = document.getElementById('email');
